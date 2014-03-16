e069edcd01d097786ff4b4f8439f24c21f996c3d
schuppo <oliver.schupp@yahoo.de>
2014-03-16 19:36:55 +0100

    more

--- ansible/scripts/add_modoboa_cron_jobs.sh
+++ ansible/scripts/add_modoboa_cron_jobs.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 if
-    ! grep "$1.*cleanlog" /var/spool/cron/root
+    ! grep "$1.*cleanlog" /var/spool/cron/postfix
 then
     echo "0 0 * * * /var/www/sites/$1/manage.py cleanlogs" >> /var/spool/cron/root
-    echo "Added \"0 0 * * * /var/www/sites/$1/manage.py cleanlogs\" to /var/spool/cron/root"
+    echo "Added \"0 0 * * * /var/www/sites/$1/manage.py cleanlogs\" to /var/spool/cron/postfix"
 else
     echo "\"0 0 * * * /var/www/sites/$1/manage.py cleanlogs\" already added."
 fi
 
 if
-    ! grep "$1.*cleanup" /var/spool/cron/root
+    ! grep "$1.*cleanup" /var/spool/cron/postfix
 then
-    echo "0 0 * * * /var/www/sites/$1/manage.py cleanup" >> /var/spool/cron/root
-    echo "Added \"0 0 * * * /var/www/sites/$1/manage.py cleanup\" to /var/spool/cron/root"
+    echo "0 0 * * * /var/www/sites/$1/manage.py cleanup" >> /var/spool/cron/postfix
+    echo "Added \"0 0 * * * /var/www/sites/$1/manage.py cleanup\" to /var/spool/cron/postfix"
 else
     echo "\"0 0 * * * /var/www/sites/$1/manage.py cleanup\" already added."
+fi
+
+if
+    ! grep "$1.*handle_mailbox_operations" /var/spool/cron/postfix
+then
+    echo "* * * * * /var/www/sites/$1/manage.py handle_mailbox_operations" >> /var/spool/cron/postfix
+    echo "Added \"* * * * * /var/www/sites/$1/manage.py handle_mailbox_operations\" to /var/spool/cron/postfix"
+else
+    echo "\"* * * * * /var/www/sites/$1/manage.py handle_mailbox_operations\" already added."
 fi
